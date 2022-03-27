ed1ab7bd15676ede9c9f56c45acee2785c501a12
tayyab34 <97193206+tayyab34@users.noreply.github.com>
2022-03-27 16:02:13 +0500

    Far Cry Survival

--- Assets/Scripts/Game Manager/EnemyManager.cs
+++ Assets/Scripts/Game Manager/EnemyManager.cs
@@ -4,12 +4,13 @@ using UnityEngine;
 
 public class EnemyManager : MonoBehaviour
 {
+
     public static EnemyManager instance;
 
     [SerializeField]
     private GameObject boar_Prefab, cannibal_Prefab;
 
-    public Transform[] cannibal_SpawnPoints,boar_SpawnPoints;
+    public Transform[] cannibal_SpawnPoints, boar_SpawnPoints;
 
     [SerializeField]
     private int cannibal_Enemy_Count, boar_Enemy_Count;
@@ -18,9 +19,10 @@ public class EnemyManager : MonoBehaviour
 
     public float wait_Before_Spawn_Enemies_Time = 10f;
 
+    // Use this for initialization
     void Awake()
     {
-        MakeInstance();    
+        MakeInstance();
     }
 
     void Start()
@@ -32,6 +34,7 @@ public class EnemyManager : MonoBehaviour
 
         StartCoroutine("CheckToSpawnEnemies");
     }
+
     void MakeInstance()
     {
         if (instance == null)
@@ -48,38 +51,48 @@ public class EnemyManager : MonoBehaviour
 
     void SpawnCannibals()
     {
+
         int index = 0;
 
         for (int i = 0; i < cannibal_Enemy_Count; i++)
         {
+
             if (index >= cannibal_SpawnPoints.Length)
             {
                 index = 0;
             }
+
             Instantiate(cannibal_Prefab, cannibal_SpawnPoints[index].position, Quaternion.identity);
 
             index++;
+
         }
 
         cannibal_Enemy_Count = 0;
+
     }
 
     void SpawnBoars()
     {
+
         int index = 0;
 
         for (int i = 0; i < boar_Enemy_Count; i++)
         {
+
             if (index >= boar_SpawnPoints.Length)
             {
                 index = 0;
             }
+
             Instantiate(boar_Prefab, boar_SpawnPoints[index].position, Quaternion.identity);
 
             index++;
+
         }
 
         boar_Enemy_Count = 0;
+
     }
 
     IEnumerator CheckToSpawnEnemies()
@@ -91,12 +104,15 @@ public class EnemyManager : MonoBehaviour
         SpawnBoars();
 
         StartCoroutine("CheckToSpawnEnemies");
+
     }
 
     public void EnemyDied(bool cannibal)
     {
+
         if (cannibal)
         {
+
             cannibal_Enemy_Count++;
 
             if (cannibal_Enemy_Count > initial_Cannibal_Count)
@@ -104,20 +120,24 @@ public class EnemyManager : MonoBehaviour
                 cannibal_Enemy_Count = initial_Cannibal_Count;
             }
 
-            else
-            {
-                boar_Enemy_Count++;
-
-                if (boar_Enemy_Count > initial_Cannibal_Count)
-                {
-                    boar_Enemy_Count = initial_Cannibal_Count;
-                }
-            }
         }
+        else
+        {
+
+            boar_Enemy_Count++;
+
+            if (boar_Enemy_Count > initial_Boar_Count)
+            {
+                boar_Enemy_Count = initial_Boar_Count;
+            }
+
+        }
+
     }
 
     public void StopSpawning()
     {
         StopCoroutine("CheckToSpawnEnemies");
     }
-}
+
+} 
